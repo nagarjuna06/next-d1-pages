ca66ce4035fd1e857c3f2c3d5bdbac20faa49f49
Nagarjuna <chennanagarjuna4@gmail.com>
2024-06-24 20:46:53 +0530

    edge

--- prisma/db.ts
+++ prisma/db.ts
@@ -2,6 +2,8 @@ import { PrismaD1 } from "@prisma/adapter-d1";
 import { PrismaClient } from "@prisma/client";
 import { getRequestContext } from "@cloudflare/next-on-pages";
 
+export const runtime = "edge";
+
 const connectDB = () => {
   const adapter = new PrismaD1(getRequestContext().env.DB);
   return new PrismaClient({ adapter });
